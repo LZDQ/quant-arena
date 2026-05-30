06ede7a5f74615b7409d1e55b01278b99d26451e
LZDQ <1486701401@qq.com>
2026-05-31 01:13:47 +0800

    Add futu test get user info

--- scripts/futu_smoke.py
+++ scripts/futu_smoke.py
@@ -2,6 +2,7 @@ from futu import OpenQuoteContext, SubType
 
 q = OpenQuoteContext(host="127.0.0.1", port=11111)
 try:
+    print(q.get_user_info())
     print(q.subscribe(["HK.00700"], [SubType.ORDER_BOOK], subscribe_push=False))
     print(q.get_order_book("HK.00700"))
 finally:
